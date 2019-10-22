ac22607f42e889f958bb27758b0afdddb7ad36cc
gebhartn <nicholas.gebhart@gmail.com>
2019-10-21 19:06:43 -0700

    feat: map trace

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
-import React, { useEffect } from 'react';
-import Axios from 'axios';
+import React from 'react';
 import MapContainer from './containers/map/MapContainer';
 
 const App = () => {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <MapContainer
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBFx7hyKdz_P_zqJCvtG_S07js4cOXupAQ"
-      />
+    <div>
+      <MapContainer />
     </div>
   );
 };
--- client/src/containers/map/MapContainer.js
+++ client/src/containers/map/MapContainer.js
@@ -1,90 +1,35 @@
-import React, { useEffect } from 'react';
-import {
-  withGoogleMap,
-  GoogleMap,
-  withScriptjs,
-  Polygon,
-} from 'react-google-maps';
-import Axios from 'axios';
+import React from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import styled from 'styled-components';
 
-const MapContainer = () => {
-  const [mapData, setMapData] = React.useState([]);
-  useEffect(() => {
-    Axios.get(
-      'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json',
-    ).then(res => {
-      setMapData(res.data.features);
+import getData from '../../temp/getData';
+
+const Wrapper = styled.div`
+  width: ${props => props.width};
+  height: ${props => props.height};
+`;
+
+const Map = () => {
+  React.useEffect(() => {
+    const map = L.map('map', {
+      center: [-10.99404, 39.75621],
+      zoom: 8,
+      zoomControl: true,
     });
+    getData().then(res => L.geoJson(res.data.features).addTo(map));
+
+    L.tileLayer(
+      `https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png`,
+      {
+        detectRetina: true,
+        maxZoom: 19,
+        maxNativeZoom: 17,
+      },
+    ).addTo(map);
   }, []);
 
-  console.log(<GoogleMap />);
-  // const renderDudes = data => {
-  //   return data.map(type => {
-  //     if (type.geometry.coordinates.length > 2) {
-  //       console.log(type);
-  //       console.log(<GoogleMap />);
-  //     }
-  //     if (type.geometry.coordinates.length === 2) {
-  //       const coords = type.geometry.coordinates[0][0];
-  //       const coordArr = [];
-  //       coords.map(coord => coordArr.push({ lat: coord[1], lng: coord[0] }));
-
-  //       return (
-  //         <Polygon
-  //           path={coordArr}
-  //           options={{
-  //             strokeColor: '#fc1e0d',
-  //             strokeOpacity: 1,
-  //             strokeWeight: 2,
-  //             icons: [
-  //               {
-  //                 icon: 'hello',
-  //                 offset: '0',
-  //                 repeat: '10px',
-  //               },
-  //             ],
-  //           }}
-  //         />
-  //       );
-  //     }
-
-  //     if (type.geometry.coordinates.length === 1) {
-  //       const coords = type.geometry.coordinates[0];
-  //       const coordArr = [];
-  //       coords.map(coord => coordArr.push({ lat: coord[1], lng: coord[0] }));
-
-  //       return (
-  //         <Polygon
-  //           path={coordArr}
-  //           options={{
-  //             strokeColor: '#fc1e0d',
-  //             strokeOpacity: 1,
-  //             strokeWeight: 2,
-  //             icons: [
-  //               {
-  //                 icon: 'hello',
-  //                 offset: '0',
-  //                 repeat: '10px',
-  //               },
-  //             ],
-  //           }}
-  //         />
-  //       );
-  //     }
-  //   });
-  // };
-
-  return (
-    <>
-      <GoogleMap
-        defaultZoom={4}
-        defaultCenter={{ lng: 61.210817, lat: 35.650072 }}
-        // data={addGeoJson(mapData)}
-      />
-    </>
-  );
+  return <Wrapper width="100vw" height="100vh" id="map" />;
 };
 
-const WrappedMap = withScriptjs(withGoogleMap(MapContainer));
-
-export default WrappedMap;
+export default Map;
