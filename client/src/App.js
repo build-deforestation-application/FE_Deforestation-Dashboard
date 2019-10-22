6cc2a31b528a5d0f15bd56237c7ff07eec69862a
frescocodes <hueydakotah@gmail.com>
2019-10-22 13:56:28 -0400

    dakotah-huey

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import MapContainer from './containers/map/MapContainer';
+import { Switch, Route } from 'react-router-dom'
+
+// components
+import Private from './utils/Private'
+import LoginForm from './components/forms/login/LoginForm'
 
 const App = () => {
   return (
-    <div>
-      <MapContainer />
-    </div>
+    <>
+      <div>
+        <Switch>
+          <Route path='/' component={LoginForm} />
+          <Private path='/dashboard' component={MapContainer} />
+        </Switch>
+        {/* <MapContainer /> */}
+      </div>
+    </>
   );
 };
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-
-import store from './reducers';
 
 import App from './App';
 
+import { BrowserRouter } from 'react-router-dom'
+
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
   document.getElementById('root'),
 );
