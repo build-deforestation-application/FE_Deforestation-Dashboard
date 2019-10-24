39a8a303f86ccc45b1b59b8b324312f94466ed3b
frescocodes <hueydakotah@gmail.com>
2019-10-24 01:48:34 -0400

    added selector, chart data displayed, still ugly for now

--- client/src/components/panels/Display.js
+++ client/src/components/panels/Display.js
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react';
 import ControlsContext from '../../contexts/ControlsContext';
+import makeArray from '../../selectors/selectors'
+
+import Table from './Table'
 
 export default () => {
 
@@ -14,23 +17,24 @@ export default () => {
   
   console.log('YEARS',years)
 
-  const formatData = (array) => {
-    return Object.entries(array).filter((x) => x[0] !== "Country" && x[0] !== "Code");
-}
+  
 
   return (
       <ControlsContext.Consumer>
         {() => {
           return (
             <>
-              <table>
-                <thead>
-                  {years.map(year => <th>{year}</th>)}
-                </thead>
-                <tbody>
-
-                </tbody>
-              </table>
+            {data.map((country, index) => {
+              const year = makeArray(country, ['Country', 'Code'])
+              return (
+                <Table 
+                key={index}
+                country={country.Country}
+                code={country.Code}
+                year={year}
+                />
+              )
+            })}
             </>
           )
         }}
