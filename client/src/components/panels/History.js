f6e09a9a0d049f4c71f4c182e73658f09c665155
frescocodes <hueydakotah@gmail.com>
2019-10-24 00:53:01 -0400

    add history feature, more context stuff

--- client/src/components/panels/History.js
+++ client/src/components/panels/History.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
+import ControlsContext from '../../contexts/ControlsContext'
+import axios from '../../utils/axios'
 
 export default () => {
+
+    const { history, setHistory } = useContext(ControlsContext)
+
+    useEffect (() => {
+        axios()
+        .get('/history')
+        .then(res => setHistory(res))
+        .catch(err => console.log(err))
+    })
+    
     return (
-        <h1>history</h1>
+
+        <ControlsContext.Consumer>
+            {() => {
+                return (
+                    <h1>{history}</h1>
+                )
+            }}
+        </ControlsContext.Consumer>
+
     )
 }
--- client/src/containers/dashboard/Dashboard.js
+++ client/src/containers/dashboard/Dashboard.js
@@ -2,23 +2,24 @@ import React, { useState, useEffect } from 'react';
 import fetchData from '../../services/queryApi';
 
 // components
-import Search from '../../components/panels/Search';
 import Display from '../../components/panels/Display';
 import Info from '../../components/panels/Info';
 import Bottom from '../../components/panels/Bottom';
 import ControlsContext from '../../contexts/ControlsContext';
 
 export default () => {
-  const [data, setData] = useState([]);
-  const [temp, setTemp] = useState({});
-  // does history need temp/setTemp for onSubmit?
+  const [ data, setData ] = useState([]);
+  const [ temp, setTemp ] = useState({});
+  const [ history, setHistory ] = useState([])
+  
+
 
   useEffect(() => {
     fetchData().then(res => setData(res.data));
   }, []);
 
   return (
-    <ControlsContext.Provider value={{data, setData, temp, setTemp}}>
+    <ControlsContext.Provider value={{ data, setData, temp, setTemp, history, setHistory }}>
       <h1>Data is here</h1>
       <Info />{/* data temp */}
       <Display />{/* data temp */}
