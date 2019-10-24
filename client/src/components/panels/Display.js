e27f3bf4c9a9ffff2de7cac7d34c30064fe79424
frescocodes <hueydakotah@gmail.com>
2019-10-24 01:13:44 -0400

    starting tables

--- client/src/components/panels/Display.js
+++ client/src/components/panels/Display.js
@@ -4,10 +4,36 @@ import ControlsContext from '../../contexts/ControlsContext';
 export default () => {
 
   const { temp, data } = useContext(ControlsContext)
+  console.log('DATA DISPLAY:',data)
+  // {() => <pre>{JSON.stringify(data.find(x => x.Country === temp.country), null, 2)}</pre>}
+  const years = [];
+
+  for (let i = 1990; i <= 2016; i++) {
+      years.push(i);
+    }
+  
+  console.log('YEARS',years)
+
+  const formatData = (array) => {
+    return Object.entries(array).filter((x) => x[0] !== "Country" && x[0] !== "Code");
+}
 
   return (
       <ControlsContext.Consumer>
-          {() => <pre>{JSON.stringify(data.find(x => x.Country === temp.country), null, 2)}</pre>}
+        {() => {
+          return (
+            <>
+              <table>
+                <thead>
+                  {years.map(year => <th>{year}</th>)}
+                </thead>
+                <tbody>
+
+                </tbody>
+              </table>
+            </>
+          )
+        }}
       </ControlsContext.Consumer>
   );
 };
